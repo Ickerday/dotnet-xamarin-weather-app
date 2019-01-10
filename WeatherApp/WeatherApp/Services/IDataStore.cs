8d60a493a5277a979a7ec8975e21006d4f0c378f
Bartosz <elitarform@gmail.com>
2019-01-10 15:45:38 +0100

    Modify IDataStore interface

--- WeatherApp/WeatherApp/Services/IDataStore.cs
+++ WeatherApp/WeatherApp/Services/IDataStore.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WeatherApp.Services
 {
-    public interface IDataStore<T>
+    public interface IDataStore<in TKey, TValue>
     {
-        Task<bool> AddItemAsync(T item);
-        Task<bool> UpdateItemAsync(T item);
-        Task<bool> DeleteItemAsync(string id);
-        Task<T> GetItemAsync(string id);
-        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+        Task<IEnumerable<TValue>> GetAsync();
+        Task<TValue> GetAsync(TKey id);
+        Task<int> SaveAsync(TValue item);
+        Task<int> DeleteAsync(TKey id);
     }
 }
