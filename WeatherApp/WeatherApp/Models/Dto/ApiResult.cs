e057412d3348b39c4467a6f0a904693a2be11bce
Bartosz <elitarform@gmail.com>
2019-01-10 15:41:28 +0100

    Add DTO models for OpenWeatherApi

---
+++ WeatherApp/WeatherApp/Models/Dto/ApiResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherApp.Models.Dto
+{
+    public class ApiResult
+    {
+        public int Id { get; set; }
+        public Coord Coord { get; set; }
+        public IEnumerable<Weather> Weather { get; set; }
+        public string Base { get; set; }
+        public Atmosphere Atmosphere { get; set; }
+        public int Visibility { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public int Dt { get; set; }
+        public Sys Sys { get; set; }
+        public string Name { get; set; }
+        public int Cod { get; set; }
+    }
+}
---
+++ WeatherApp/WeatherApp/Models/Forecast.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Essentials;
+
+namespace WeatherApp.Models
+{
+    public class Coord
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public class Forecast : BaseEntity
+    {
+        public Location Location { get; set; }
+        
+    }
+}
