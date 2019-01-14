bc21f323e5f6cea8ecbdeb6c9bc24429021b6e68
Bartosz <elitarform@gmail.com>
2019-01-14 09:20:59 +0100

    Add Xamarin Essentials to Android MainActivity

--- WeatherApp/WeatherApp.Android/MainActivity.cs
+++ WeatherApp/WeatherApp.Android/MainActivity.cs
@@ -1,24 +1,45 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
+using System;
+using Xamarin.Essentials;
 
 namespace WeatherApp.Droid
 {
-    [Activity(Label = "WeatherApp", 
-        Icon = "@mipmap/icon", 
+    [Activity(Label = "WeatherApp",
+        Icon = "@mipmap/icon",
         Theme = "@style/MainTheme",
-        MainLauncher = true, 
+        MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            try
+            {
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
-            Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+                base.OnCreate(savedInstanceState);
+                Instance = this;
+                Xamarin.Forms.Forms.Init(this, savedInstanceState);
+                Platform.Init(this, savedInstanceState);
+
+                LoadApplication(new App());
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
