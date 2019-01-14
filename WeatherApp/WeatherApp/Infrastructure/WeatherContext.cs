0df0de2df8fd314e0f5cd38e274531b182fa63f4
Bartosz <elitarform@gmail.com>
2019-01-14 09:30:46 +0100

    Add WeatherContext

---
+++ WeatherApp/WeatherApp/Infrastructure/WeatherContext.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using WeatherApp.Models;
+using Xamarin.Essentials;
+
+namespace WeatherApp.Infrastructure
+{
+    public class WeatherContext : SQLiteAsyncConnection
+    {
+        public WeatherContext(string dbPath) : base(dbPath)
+        {
+            CreateTableAsync<Location>().Wait();
+            CreateTableAsync<Forecast>().Wait();
+        }
+    }
+}
