95edb4561d5428bec2ae270b12885ba322a265a5
Bartosz <elitarform@gmail.com>
2019-01-15 15:52:04 +0100

    Simplify WeatherService

--- WeatherApp/WeatherApp/Services/WeatherApiService.cs
+++ WeatherApp/WeatherApp/Services/WeatherApiService.cs
@@ -1,6 +1,5 @@
 ﻿using RestEase;
 using System.Threading.Tasks;
-using WeatherApp.Exceptions;
 using WeatherApp.Models.Dto;
 using Xamarin.Essentials;
 
@@ -12,10 +11,10 @@ namespace WeatherApp.Services
         string ApiKey { get; set; }
 
         [Get("weather?q={cityName}&units=metric&appid={apiKey}")]
-        Task<string> GetByCityNameAsync([Path("cityName")] string cityName);
+        Task<ApiResult> GetByCityNameAsync([Path("cityName")] string cityName);
 
         [Get("weather?q={id}&units=metric&appId={apiKey}")]
-        Task<string> GetByIdAsync([Path("id")] int id);
+        Task<ApiResult> GetByIdAsync([Path("id")] int id);
 
         [Get("weather?lat={latitude}&lon={longitude}&units=metric&appId={apiKey}")]
         Task<ApiResult> GetByLocationAsync([Path("latitude")] float latitude, [Path("longitude")] float longitude);
@@ -23,9 +22,6 @@ namespace WeatherApp.Services
 
     public class WeatherApiService
     {
-        private const string SampleKey = "b6907d289e10d714a6e88b30761fae22";
-        private const string SampleUrl = "https://samples.openweathermap.org/data/2.5";
-
         private const string ApiKey = "2069213dc25f76d153d15c301f7f013c";
         private const string BaseUrl = "https://api.openweathermap.org/data/2.5";
 
@@ -37,21 +33,13 @@ namespace WeatherApp.Services
             _client.ApiKey = ApiKey;
         }
 
-        public async Task<string> GetByCityNameAsync(string name) => await _client
-                .GetByCityNameAsync(name);
+        public async Task<ApiResult> GetByCityNameAsync(string name) =>
+            await _client.GetByCityNameAsync(name);
 
-        public async Task<string> GetByIdAsync(int id) => await _client
-            .GetByIdAsync(id);
+        public async Task<ApiResult> GetByIdAsync(int id) =>
+            await _client.GetByIdAsync(id);
 
-        public async Task<ApiResult> GetByLocationAsync(Location location)
-        {
-            var result = await _client
-                .GetByLocationAsync((float)location.Latitude, (float)location.Longitude);
-
-            if (result.Cod == 200)
-                return result;
-
-            throw new AppException($"OpenWeatherAPI returned {result.Cod}: {result.Message}");
-        }
+        public async Task<ApiResult> GetByLocationAsync(Location location) =>
+            await _client.GetByLocationAsync((float)location.Latitude, (float)location.Longitude);
     }
 }
