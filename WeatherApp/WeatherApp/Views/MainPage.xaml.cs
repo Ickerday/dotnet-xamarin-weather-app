c404fbb4b985ae759b938e340294223c0e8c380f
Bartosz <elitarform@gmail.com>
2019-01-15 15:55:01 +0100

    Add event handlers and OnStart to MainPage

--- WeatherApp/WeatherApp/Views/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/MainPage.xaml.cs
@@ -1,14 +1,47 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using WeatherApp.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace WeatherApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : TabbedPage
+    public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            SearchMaster.PreviousCitiesListView.ItemSelected += OnPreviousCitySelected;
+            SearchMaster.CityEntry.Completed += OnSearchCompleted;
+            SearchMaster.SearchButton.Clicked += OnSearchCompleted;
+        }
+
+        public void OnStart()
+        {
+            LocationDetail.OnStart();
+        }
+
+        public async void OnPreviousCitySelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (!(e.SelectedItem is SearchPageListItem item))
+                return;
+
+            await LocationDetail.OnInputFromMasterPageAsync(item.Name);
+            SearchMaster.PreviousCitiesListView.SelectedItem = null;
+            IsPresented = false;
+        }
+
+        public async void OnSearchCompleted(object sender, EventArgs e)
+        {
+            var cityEntry = SearchMaster.CityEntry.Text;
+            if (string.IsNullOrEmpty(cityEntry))
+                return;
+
+            SearchMaster.CityEntry.Text = string.Empty;
+
+            await LocationDetail.OnInputFromMasterPageAsync(cityEntry);
+            IsPresented = false;
         }
     }
 }
