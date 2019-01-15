8fc57a5c16f5ab3b9d0a85eb335e681c5b438291
Bartosz <elitarform@gmail.com>
2019-01-15 15:53:41 +0100

    Add searched cities history

--- WeatherApp/WeatherApp/ViewModels/SearchPageViewModel.cs
+++ WeatherApp/WeatherApp/ViewModels/SearchPageViewModel.cs
@@ -1,12 +1,37 @@
 ﻿using MvvmHelpers;
+using System.Collections.ObjectModel;
 
 namespace WeatherApp.ViewModels
 {
+    public class SearchPageListItem
+    {
+        public string Name { get; set; }
+
+        public SearchPageListItem(string name = "") =>
+            Name = name;
+    }
+
     public class SearchPageViewModel : BaseViewModel
     {
+        public ObservableCollection<SearchPageListItem> PreviousCities { get; set; }
+
         public SearchPageViewModel()
         {
-            Title = "Find a city manually";
+            Title = "Find city";
+            PreviousCities = new ObservableCollection<SearchPageListItem>
+            {
+                new SearchPageListItem("Kyoto, JP"),
+                new SearchPageListItem("Seattle, US")
+            };
+        }
+
+        public void AddToPreviousCities(string cityName)
+        {
+            if (string.IsNullOrEmpty(cityName))
+                return;
+
+            PreviousCities.Add(new SearchPageListItem(cityName));
+            OnPropertyChanged(nameof(PreviousCities));
         }
     }
 }
