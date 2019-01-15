dfbb69d46059f43742147a354e36e9cdfa13fbcd
Bartosz <elitarform@gmail.com>
2019-01-15 15:55:22 +0100

    Add event handler to SearchPage

--- WeatherApp/WeatherApp/Views/SearchPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/SearchPage.xaml.cs
@@ -1,25 +1,29 @@
 ﻿using System;
-using System.Threading.Tasks;
 using WeatherApp.ViewModels;
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace WeatherApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SearchPage : ContentPage
+    public partial class SearchPage
     {
-        private SearchPageViewModel _viewModel;
+        private readonly SearchPageViewModel _viewModel;
 
         public SearchPage()
         {
             BindingContext = _viewModel = new SearchPageViewModel();
             InitializeComponent();
+
+            CityEntry.Completed += OnSearchCompleted;
         }
 
-        public async void OnSearchButtonClickedAsync(object sender, EventArgs args)
+        private void OnSearchCompleted(object sender, EventArgs e)
         {
-            await Task.CompletedTask;
+            var cityEntry = CityEntry.Text;
+            if (string.IsNullOrEmpty(cityEntry))
+                return;
+
+            _viewModel.AddToPreviousCities(cityEntry);
         }
     }
 }
