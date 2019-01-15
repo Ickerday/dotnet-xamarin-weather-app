583806b51cee3f35ea2acc570e91a9f4f89df7c3
Bartosz <elitarform@gmail.com>
2019-01-15 15:49:09 +0100

    Add Seed

--- WeatherApp/WeatherApp/Infrastructure/WeatherContext.cs
+++ WeatherApp/WeatherApp/Infrastructure/WeatherContext.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System;
 using WeatherApp.Models;
 
 namespace WeatherApp.Infrastructure
@@ -8,6 +9,38 @@ namespace WeatherApp.Infrastructure
         public WeatherContext(string dbPath) : base(dbPath)
         {
             CreateTableAsync<Forecast>().Wait();
+
+#if DEBUG
+            Seed();
+#endif
+        }
+
+        private void Seed()
+        {
+            var seed = new[]
+            {
+                new Forecast
+                {
+                    Name = "Kraków",
+                    Country = "PL",
+                    CheckedAt = DateTime.Now,
+                    Temperature = 10,
+                    Humidity = 10,
+                    Latitude = 100,
+                    Longitude = 100,
+                },
+                new Forecast
+                {
+                    Name = "Berlin",
+                    Country = "DE",
+                    CheckedAt = DateTime.Now + TimeSpan.FromHours(1),
+                    Temperature = 30,
+                    Humidity = 100,
+                    Latitude = 10,
+                    Longitude = -100
+                }
+            };
+            InsertAllAsync(seed);
         }
     }
 }
